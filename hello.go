8184845e73bd1a17f1b5dfbc263b945be075dc0d
yawzyag <yesid.dev93@gmail.com>
2020-04-12 19:47:21 -0500

    [Feature] test for different languajes

--- hello.go
+++ hello.go
@@ -2,16 +2,27 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const prefixEnglish = "Hello, "
+const prefixSpanish = "Hola, "
+const prefixFrench = "Bonjour, "
 
 // Hello prints Hello, world.
-func Hello(s string) string {
+func Hello(s string, languaje string) string {
+	printPrefix := prefixEnglish
+	if languaje == spanish {
+		printPrefix = prefixSpanish
+	}
+	if languaje == french {
+		printPrefix = prefixFrench
+	}
 	if s == "" {
 		s = "world"
 	}
-	return prefixEnglish + s
+	return printPrefix + s
 }
 
 func main() {
-	fmt.Println(Hello("test"))
+	fmt.Println(Hello("test", "Spanish"))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,24 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello + variable passed to function", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertMessage(t, got, want)
 	})
 
 	t.Run("Say hello world if no variable passed to function", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertMessage(t, got, want)
+	})
+	t.Run("Say Hola, segundo parametro para lenguaje", func(t *testing.T) {
+		got := Hello("Yesid", "Spanish")
+		want := "Hola, Yesid"
+		assertMessage(t, got, want)
+	})
+	t.Run("Say Bonjour, second parameter languaje", func(t *testing.T) {
+		got := Hello("Yesid", "French")
+		want := "Bonjour, Yesid"
 		assertMessage(t, got, want)
 	})
 }
